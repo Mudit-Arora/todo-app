011ba4ae0233ac9db67fa5a3fdb69ba40657bef8
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2023-05-13 02:03:22 -0700

    comment

--- gui.py
+++ gui.py
@@ -6,5 +6,6 @@ input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
 window = sg.Window("My To-Do App", layout= [[label], [input_box, add_button]])
+# [] means one single row, [[c],[a,b]] will have c in first row and a & b in 2nd row
 window.read()
 window.close()
